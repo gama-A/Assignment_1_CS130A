323d39d8a2cf7abc51a6c9db02b7664c36698827
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-04-07 20:59:19 -0700

    main executable created

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Gama Aristondo, 8404071
+//
+// Coding Assignment 1
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// #include CLASS_NAME.h
+
+using namespace std;
+
+int main() {
+    return 0;
+}
