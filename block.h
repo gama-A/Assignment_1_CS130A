c8133ace22fef255ff5675466198c3212ecf2b2d
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-04-07 21:34:43 -0700

    working on header file

--- block.h
+++ block.h
@@ -5,9 +5,27 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <iostream>
+#include <string>
+
 class block
 {
     public:
+        Block();
+        // Constructor for block transaction
+
+        ~BlockChain();
+        // Decstructor for block transaction
+
+        add(int amount, std::string sender, std::string receiver);
+        // Adds a transaction to the chain
+        // View as a constructor
+
+        int getBalance(std::string name);
+        // Gets the balance of the given person 
+
+        std::string printChain();
+        // Prints the entire contents of the chain
 
     private:
         block *prev;       // points to previous block transaction
